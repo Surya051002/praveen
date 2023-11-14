26aedbdef5db4b83239d43e0e2cee4345e043b89
sanjay77826 <sureshsk7323@gmail.com>
2023-11-14 18:36:40 +0530

    first

---
+++ Home.js
@@ -0,0 +1,7 @@
+// Simulated function to add product to cart
+function addToCart(productId) {
+    // You can implement logic to add the product to the cart here
+    console.log(`Product ${productId} added to cart`);
+    alert('Product added to cart!');
+  }
+  
